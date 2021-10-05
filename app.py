383f6dd336c497c1e8c5893d72f4e05ddcdeb05c
Ergi <ergikame@gmail.com>
2021-10-05 16:20:25 +0200

    added register and login features (no database yet)

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+import secret_key
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = secret_key.SECRET_KEY
 
 posts = [
     {
@@ -19,13 +23,32 @@ posts = [
 
 @app.route('/')
 @app.route('/home')
-def hello_world():  # put application's code here
+def home():  # put application's code here
     return render_template("home.html", posts=posts)
 
 @app.route('/about')
 def about():
     #title sets the name at our tab in browser
     return render_template("about.html", title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'test@flask.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful! Please check username and password.', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
